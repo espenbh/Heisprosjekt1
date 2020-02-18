1e65337a13bbd84a9c2c39fa5e0531dcefaf9bcc
Espen Høgstedt <espebh@stud.ntnu.no>
2020-02-18 15:57:45 +0100

    endra controlfiles

--- source/controlFiles.c
+++ source/controlFiles.c
@@ -1,11 +1,18 @@
 #include <stdio.h>
+#include "controlFiles.h"
 
-int CheckArriveFloor(Array currentFloors) {
+int CheckArriveFloor(int currentFloors[]) {
   for(i=0;i<HARDWARE_NUMBER_OF_FLOORS;i++){
     if(hardware_read_floor_sensor(i) > currentFloors[i]){
-      currentFloors[i]=1
-      return 1;
+      currentFloors[i]=1;
+      return 1
     }
-    elseif(hardware_read_floor_sensor(i)<currentFloors[i])
+    elseif(hardware_read_floor_sensor(i) < currentFloors[i]) {
+      currentFloors[i]=0;
+      return 0;
+    }
+    else{
+      return 0
+    }
   }
 }
