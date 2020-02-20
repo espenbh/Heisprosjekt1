00a6bb5efea4a519b9a9e34856250474ca6d175c
Espen Høgstedt <espebh@stud.ntnu.no>
2020-02-20 12:42:27 +0100

    lagde timer-funk

--- source/timer_kladd.c
+++ source/timer_kladd.c
@@ -1,8 +1,25 @@
-#include <time.h>
+#include "time.h"
 #include <stdio.h>
+volatile time_t TIMER;
 
-void StartTimer() {
-
+volatile void StartTimer() {
+  TIMER=clock();
 }
-int TimerCount()
-void Stoptimer()
+volatile int TimerCount(){
+  return ((clock()-TIMER)/CLOCKS_PER_SEC);
+}
+
+volatile void Stoptimer() {
+  TIMER=0;
+}
+
+int main(){
+  StartTimer();
+  printf("Klokka starter, og er %d\n", TIMER);
+  int a=0;
+  while(TimerCount()<3){
+    a=1;
+  }
+  printf("Timeren har gått i tre sekunder, og er %d", TIMER+TimerCount());
+  return 0;
+}
