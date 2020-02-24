1476656655254737f09e64b560b02de2775b4c79
Espen Høgstedt <espebh@stud.ntnu.no>
2020-02-24 13:30:13 +0100

    Comments added

--- source/timer.h
+++ source/timer.h
@@ -1,7 +1,18 @@
+/**
+* @file
+* @brief Functions to keep track of how long certain operations has last. */
+
 #include <stdio.h>
 #include <time.h>
 #include <stdbool.h>
 
+/**
+* @brief Starts the timer from scratch.
+* @param[out] TIMER Global timer variable. */
 void StartTimer();
+
+/**
+* @brief Returns the time since the last call to @f StartTimer().
+* @param[in] TIMER Global timer variable.
+* @return Returns the number of seconds since the last call to @f StartTimer(). */
 int TimerCount();
-void Stoptimer();
--- source/variables.h
+++ source/variables.h
@@ -1,21 +1,40 @@
+/**
+* @file
+* @brief Define global variables. */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 #include "hardware.h"
 
+/**
+* @brief Define a struct to keep track of current and previous floor.
+* @param current Holds the current floor, -1 if moving.
+* @param last Holds the last floor the elevator were at.
+*/
 typedef struct {
 	int current;
 	int last;
 } Floor;
 extern Floor FLOOR;
 
+/**
+* @brief Define a struct to keep track of current and previous direction.
+* @param current Holds the current direction.
+* @param last Holds the last direction the elevator had when it was moving.
+*/
 typedef struct {
 	HardwareMovement current;
 	HardwareMovement last;
 } Direction;
 extern Direction DIRECTION;
-//extern int currentFloors[4];
+
+/**
+* @brief A matrix that keeps track of orders. */
 extern int MASTER_MATRIX[3][4];
 extern time_t TIMER;
-//extern int prevDirection;
+
+/**
+* @breif A flag thats high when the last order
+* the elevator received was stop.*/
 extern int hasStopped;
